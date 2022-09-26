0b1c7306dfbf8a312d737110162794e1c8aa88a0
bueze_nwokolo <nwokolobueze@gmail.com>
2022-09-26 17:52:30 -0600

    added methods for preOrder and PostOrder iterative traversal of Binary tree

---
+++ src/trees/BinaryTree.java
@@ -0,0 +1,103 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("InorderTraversal Output");
+        System.out.println(inorderTraversal(root));
+        System.out.println();
+
+        System.out.println("PreOrderTraversal Output");
+        System.out.println(preOrderTraversal(root));
+        System.out.println();
+
+        System.out.println("PostOrderTraversal Output");
+        System.out.println(postOrderTraversal(root));
+        System.out.println();
+    }
+    /*
+    Below are the logic for iterative traversals of Binary Tree Data Structures
+     */
+    private static ArrayList<Integer> preOrderTraversal(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        stack.push(current);
+
+        while (!stack.isEmpty()) {
+            current = stack.pop();
+            result.add(current.val);
+
+            if (current.right != null) stack.push(current.right);
+            if (current.left != null) stack.push(current.left);
+        }
+
+        return result;
+    }
+
+    private static ArrayList<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode temp = root;
+        while (!stack.isEmpty() || temp != null) {
+            if (temp != null) {
+                stack.push(temp);
+                temp = temp.left;
+            } else {
+                temp = stack.pop();
+                result.add(temp.val);
+                temp = temp.right;
+            }
+        }
+        return result;
+    }
+
+    private static ArrayList<Integer> postOrderTraversal(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (!stack.isEmpty() || current != null) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                // The variable temp ensures that we check the right
+                // of the current node before deciding to process
+                // the left node
+                TreeNode temp = stack.peek().right;
+                if (temp == null) {
+                    temp = stack.pop();
+                    result.add(temp.val);
+                    // this logic takes care of skewed binary trees
+                    while (!stack.isEmpty() && temp == stack.peek().right) {
+                        temp = stack.pop();
+                        result.add(temp.val);
+                    }
+                } else {
+                    current = temp;
+                }
+            }
+        }
+        return result;
+    }
+}
